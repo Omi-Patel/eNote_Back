4bce76a3472b01a8d3443ffa6d7c5d7e87500a9d
Om Patel <omupatel7113@gmail.com>
2024-04-12 18:59:24 +0530

    Backend

--- database/db.js
+++ database/db.js
@@ -1,18 +1,11 @@
-// mongodb+srv://omipatel7113:<password>@merndatabase.6u2jzyu.mongodb.net/
-// "mongodb+srv://omipatel7113:<password>@merndatabase.6u2jzyu.mongodb.net/merndatabase",
+
 
 import { connect } from "mongoose";
 
-// const dns = require('dns');
-// import dns from "dns";
-// Set default result order for DNS resolution
-// dns.setDefaultResultOrder("ipv4first");
 
 const connectToMongo = async () => {
   try {
-    await connect(
-      "mongodb+srv://omipatel7113:omipatel7113@merndatabase.6u2jzyu.mongodb.net/eNotebook"
-    );
+    await connect(process.env.MONGO_URI);
     console.log("Connection Successfull!!");
   } catch (error) {
     console.log(error);
